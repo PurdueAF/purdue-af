0169fd157e9a39a2c3e3f3b4898cccbae836fb66
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2023-07-06 15:13:45 -0400

    fix

--- docker/cmsaf-alma8/scripts/create-symlinks.sh
+++ docker/cmsaf-alma8/scripts/create-symlinks.sh
@@ -1,5 +1,5 @@
 NEW_HOME=/home/$NB_USER
-if [ ! -d eos-purdue ]; then ln -s /eos/purdue/ $NEW_HOME/eos-purdue; fi;
+if [ ! -L eos-purdue ]; then ln -sf /eos/purdue/ $NEW_HOME/eos-purdue; fi;
 if [ -L depot ]; then rm -rf depot; fi;
 if [ -f depot ]; then rm -rf depot; fi;
 if [ ! -d depot ]; then mkdir depot; fi;
@@ -8,6 +8,12 @@ if [ ! -L depot/users ]; then ln -s /depot/cms/users $NEW_HOME/depot/users; fi;
 projects=("hmm" "top" "hh")
 for project in "${projects[@]}"; do
     if [ ! -L "depot/$project" ]; then
-        ln -s "/depot/cms/$project" "$NEW_HOME/depot/$project"
+        target="/depot/cms/$project"
+        link="$NEW_HOME/depot/$project"
+        if [ -e "$link" ]; then
+            echo "Skipping $link as it already exists."
+        else
+            ln -s "$target" "$link"
+        fi
     fi
 done
