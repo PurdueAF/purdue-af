97f5ea6e1c88e6848babf9c8aefc4f7bfe167861
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2024-03-22 13:44:48 -0400

    clean up custom Dask Gateway deployment

--- dask-gateway/install-k8s-slurm.sh
+++ dask-gateway/install-k8s-slurm.sh
@@ -1,6 +1,7 @@
-helm upgrade --install dask-gateway-k8s-slurm helm-chart\
- --repo=https://helm.dask.org --install \
- --version 2023.9.0-purdue.v2 \
+# helm
+helm upgrade  dask-gateway-k8s-slurm dask-gateway\
+ --repo=https://helm.dask.org/ --install \
+ --version 2023.9.0 \
  --namespace cms \
  --values config-k8s-slurm.yaml \
- --post-renderer ./kustomize --debug --dry-run
+ --post-renderer ./kustomize
--- dask-gateway/install.sh
+++
@@ -1,6 +0,0 @@
-helm upgrade --install dask-gateway helm-chart\
- --repo=https://helm.dask.org --install \
- --version 2023.9.0-purdue.v2 \
- --namespace cms \
- --values config.yaml
- 
