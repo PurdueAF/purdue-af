d7e572ea829d4dc80b56a8741b87974dda09707c
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2024-09-19 13:20:21 -0400

    Dask Gateway fixes & remove HATS-related components

--- dask-gateway/install-hats.sh
+++
@@ -1,7 +0,0 @@
-helm upgrade --install dask-gateway-hats dask-gateway\
- --repo=https://helm.dask.org --install \
- --version 2024.1.0 \
- --namespace cms \
- --values config-hats.yaml \
- --post-renderer ./kustomize-hats
- 
