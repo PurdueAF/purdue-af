2ee2bb23908005b1b705008960479be74c05fbaf
kondratyevd <kondratyev.d.95@gmail.com>
2025-02-04 11:48:18 -0500

    add ROOT and Combine paths to default kernel

--- jupyterhub/docker/cmsaf-alma8/scripts/init-conda-kernels.sh
+++ jupyterhub/docker/cmsaf-alma8/scripts/init-conda-kernels.sh
@@ -9,14 +9,15 @@ conda run -p /depot/cms/kernels/python3 ipython kernel install \
     --prefix=/opt/conda --name="python3"  --display-name "Python3 kernel (default)"
 
 kernel_path="/opt/conda/share/jupyter/kernels/python3/"
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/depot/cms/purdue-af/lhapdf/lib
-PYTHONPATH=$PYTHONPATH:/depot/cms/purdue-af/lhapdf/lib/python3.10/site-packages
-PATH=$PATH:/depot/cms/purdue-af/lhapdf/bin
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/depot/cms/purdue-af/lhapdf/lib:/depot/cms/purdue-af/combine/HiggsAnalysis/CombinedLimit/build/lib
+PYTHONPATH=$PYTHONPATH:/depot/cms/purdue-af/lhapdf/lib/python3.10/site-packages:/depot/cms/purdue-af/combine/HiggsAnalysis/CombinedLimit/build/lib/python
+PATH=/depot/cms/kernels/python3/bin/:/depot/cms/purdue-af/combine/HiggsAnalysis/CombinedLimit/build/bin:$PATH:/depot/cms/purdue-af/lhapdf/bin
+CPLUS_INCLUDE_PATH=/depot/cms/kernels/python3/x86_64-conda-linux-gnu/sysroot/usr/include
 
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH" \
 PYTHONPATH="$PYTHONPATH" \
 PATH="$PATH" \
-    jq '.env = {"PATH": env.PATH, "PYTHONPATH": env.PYTHONPATH, "LD_LIBRARY_PATH": env.LD_LIBRARY_PATH}' \
+    jq '.env = {"PATH": env.PATH, "PYTHONPATH": env.PYTHONPATH, "LD_LIBRARY_PATH": env.LD_LIBRARY_PATH, "CPLUS_INCLUDE_PATH": env.CPLUS_INCLUDE_PATH}' \
     "$kernel_path/kernel.json" > tmp_kernel.json
 mv tmp_kernel.json "$kernel_path/kernel.json"
 
