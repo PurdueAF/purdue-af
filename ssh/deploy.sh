bf7eb8d7fef7b9e82567baa57cbb5faa6d72bb89
kondratyevd <kondratyev.d.95@gmail.com>
2025-03-13 16:32:11 -0400

    cleanup

--- ssh/deploy.sh
+++
@@ -1,6 +0,0 @@
-helm upgrade --install cmshub-ssh \
-   --repo https://yuvipanda.github.io/jupyterhub-ssh/ jupyterhub-ssh \
-   --version 0.0.1-0.dev.git.142.h402a3d6 \
-   --values values.yaml \
-   --namespace cms \
-   --post-renderer ./kustomize
