d2022d86b5103ef9b217e5800525b2505e29a947
kondratyevd <kondratyev.d.95@gmail.com>
2025-03-13 14:16:33 -0400

    cleanup and move another Dask Gateway instance to Flux

--- dask-gateway/install-k8s-slurm.sh
+++
@@ -1,7 +0,0 @@
-# helm
-helm upgrade  dask-gateway-k8s-slurm dask-gateway\
- --repo=https://helm.dask.org/ --install \
- --version 2023.9.0 \
- --namespace cms \
- --values config-k8s-slurm.yaml \
- --post-renderer ./kustomize-k8s-slurm
