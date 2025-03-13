528ce94054b9af0ad763c4dd7d5a2bf7a174acea
kondratyevd <kondratyev.d.95@gmail.com>
2025-03-13 14:44:43 -0400

    cleanup

--- dask-gateway/install-k8s.sh
+++
@@ -1,7 +0,0 @@
-helm upgrade --install dask-gateway-k8s dask-gateway\
- --repo=https://helm.dask.org \
- --version 2023.9.0 \
- --namespace cms \
- --values config-k8s.yaml \
- --post-renderer ./kustomize-k8s
- 
