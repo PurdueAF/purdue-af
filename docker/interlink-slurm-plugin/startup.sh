78bae27f274e2ecc20e5b773ee1c8c7984268c6a
kondratyevd <kondratyev.d.95@gmail.com>
2025-07-07 17:03:01 -0400

    don't start slurmd

--- docker/interlink-slurm-plugin/startup.sh
+++ docker/interlink-slurm-plugin/startup.sh
@@ -11,7 +11,6 @@ mkdir -p /etc/munge/
 cp /etc/secrets/munge/munge.key /etc/munge/
 chown munge:munge /etc/munge/munge.key
 chmod 400 /etc/munge/munge.key
-sudo su -l munge -s /usr/sbin/munged
 
 # Configure Slurm to use maximum available processors and memory
 # and start required services
@@ -20,7 +19,7 @@ sed -i "s/<<HOSTNAME>>/$(hostname)/" /etc/slurm/slurm.conf
 sed -i "s/<<CPU>>/$(nproc)/" /etc/slurm/slurm.conf
 sed -i "s/<<MEMORY>>/$(if [[ "$(slurmd -C)" =~ RealMemory=([0-9]+) ]]; then echo "${BASH_REMATCH[1]}"; else exit 100; fi)/" /etc/slurm/slurm.conf
 service munge start
-service slurmd start
+# service slurmd start
 service slurmctld start
 SCRIPT
 
