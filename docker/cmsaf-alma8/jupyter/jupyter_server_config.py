b83b477f26743e3f2c5d08a6ea8e20e8b49586a7
kondratyevd <kondratyev.d.95@gmail.com>
2025-08-05 23:57:48 -0400

    disable xsrf check

--- docker/cmsaf-alma8/jupyter/jupyter_server_config.py
+++ docker/cmsaf-alma8/jupyter/jupyter_server_config.py
@@ -10,6 +10,7 @@ from jupyter_core.paths import jupyter_data_dir
 c = get_config()  # noqa: F821
 c.ServerApp.ip = "0.0.0.0"
 c.ServerApp.open_browser = False
+c.ServerApp.disable_check_xsrf = True
 
 # to output both image/svg+xml and application/pdf plot formats in the notebook file
 c.InlineBackend.figure_formats = {"png", "jpeg", "svg", "pdf"}
