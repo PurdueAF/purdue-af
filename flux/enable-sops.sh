e8be1154c8cdb39c478a769953a23cd1f40e023a
GitHub Action <action@github.com>
2025-08-07 20:44:29 +0000

    Apply shfmt shell script formatting

--- flux/enable-sops.sh
+++ flux/enable-sops.sh
@@ -1,7 +1,7 @@
 # age-keygen -o ~/.config/sops/age/keys.txt
 # cat ~/.config/sops/age/keys.txt | grep 'public key'
 kubectl -n cms create secret generic sops-age \
-    --from-file=age.agekey=$HOME/.config/sops/age/keys.txt
+	--from-file=age.agekey=$HOME/.config/sops/age/keys.txt
 
-# in-place encryption: 
+# in-place encryption:
 # sops -e -i path/to/secret.yaml
