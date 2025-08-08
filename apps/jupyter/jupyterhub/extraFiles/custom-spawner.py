94cbde3978d59475f4c67b87bc8a553170fc4e0f
kondratyevd <kondratyev.d.95@gmail.com>
2025-08-08 16:36:40 -0400

    trying to fix jh

--- apps/jupyter/jupyterhub/extraFiles/custom-spawner.py
+++ apps/jupyter/jupyterhub/extraFiles/custom-spawner.py
@@ -59,14 +59,6 @@ c.JupyterHub.authenticator_class = PurdueCILogonOAuthenticator
 c.PurdueCILogonOAuthenticator.post_auth_hook = passthrough_post_auth_hook
 
 if os.environ["POD_NAMESPACE"] == "cms":
-    # c.KubeSpawner.service_account = "dask-sa"
-    # c.KubeSpawner.automount_service_account_token = True
-    # The current environment and dask configuration via environment
-    # export DASK_DISTRIBUTED__DASHBOARD_LINK=/user/$NB_USER/proxy/8787/status
-    # export DASK_GATEWAY__AUTH__TYPE=jupyterhub
-    # export DASK_GATEWAY__CLUSTER__OPTIONS__IMAGE={JUPYTER_IMAGE_SPEC}
-    # export DASK_GATEWAY__PUBLIC_ADDRESS=/services/dask-gateway/
-    # export DASK_ROOT_CONFIG=/opt/conda/etc
     c.KubeSpawner.environment.setdefault(
         "DASK_GATEWAY__ADDRESS", "http://dask-gateway-k8s-slurm.geddes.rcac.purdue.edu"
     )
@@ -94,8 +86,3 @@ c.KubeSpawner.environment.setdefault(
     "DASK_LABEXTENSION__FACTORY__KWARGS__PUBLIC_ADDRESS",
     "https://dask-gateway-k8s-slurm.geddes.rcac.purdue.edu",
 )
-
-# if ("-cern" in os.environ["NB_USER"]) or ("-fnal" in os.environ["NB_USER"]):
-#     c.KubeSpawner.environment.setdefault("DASK_LABEXTENSION__FACTORY__KWARGS__ADDRESS", "http://dask-gateway-k8s.geddes.rcac.purdue.edu")
-#     c.KubeSpawner.environment.setdefault("DASK_LABEXTENSION__FACTORY__KWARGS__PROXY_ADDRESS", "api-dask-gateway-k8s.cms.geddes.rcac.purdue.edu:8000")
-#     c.KubeSpawner.environment.setdefault("DASK_LABEXTENSION__FACTORY__KWARGS__PUBLIC_ADDRESS", "https://dask-gateway-k8s.geddes.rcac.purdue.edu")
