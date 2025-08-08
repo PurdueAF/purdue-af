5fab3068ddf29ba0a4b0c7832384d5e7a7541246
kondratyevd <kondratyev.d.95@gmail.com>
2025-08-08 19:12:35 -0400

    delete jupyterhub directory

--- jupyterhub/update-values.sh
+++
@@ -1,4 +0,0 @@
-helm upgrade --cleanup-on-fail --install cmshub jupyterhub/jupyterhub --version 3.2.1 \
-	--namespace cms --values values.yaml \
-	--set-file hub.extraFiles.00-custom-spawner.stringData=extra_config/custom-spawner.py \
-	--set-file hub.extraFiles.01-set-user-info.stringData=extra_config/set-user-info.py
