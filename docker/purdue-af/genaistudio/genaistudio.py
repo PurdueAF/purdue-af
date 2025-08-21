2537e207c5baab69b582b082e053241a93648406
kondratyevd <kondratyev.d.95@gmail.com>
2025-08-20 20:27:49 -0400

    fix genaistudio

--- docker/purdue-af/genaistudio/genaistudio.py
+++ docker/purdue-af/genaistudio/genaistudio.py
@@ -7,16 +7,20 @@ class PurdueGenAIStudioProvider(BaseProvider, ChatOpenAI):
     id = "genaistudio"
     name = "PurdueGenAIStudio"
     models = [
+        # "gemma3:27b",
+        # "llama3.1:latest",
+        # "llama3.2:latest",
+        # "llama3.3:70b",
         "purdue-cms-af",
     ]
-    model_id_key = "model"
+    model_id_key = "model_name"
     pypi_package_deps = ["langchain_openai"]
     auth_strategy = EnvAuthStrategy(
-        name="GENAISTUDIO_API_KEY", keyword_param="api_key"
+        name="GENAISTUDIO_API_KEY", keyword_param="openai_api_key"
     )
 
     def __init__(self, **kwargs):
-        super().__init__(base_url="https://genai.rcac.purdue.edu/api", **kwargs)
+        super().__init__(openai_api_base="https://genai.rcac.purdue.edu/api", **kwargs)
 
     @classmethod
     def is_api_key_exc(cls, e: Exception):
