f536364273bd4e571614db6b66a6270b7c2ab630
GitHub Action <action@github.com>
2026-04-27 15:20:33 +0000

    Apply black and isort formatting

--- docker/purdue-af/jupyter/jupyter_server_config.py
+++ docker/purdue-af/jupyter/jupyter_server_config.py
@@ -1,10 +1,10 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 # mypy: ignore-errors
+import logging
 import os
 import stat
 import subprocess
-import logging
 
 from jupyter_core.paths import jupyter_data_dir
 
